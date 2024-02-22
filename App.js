1ce1c30540d2f3ea8368ef24655fc17e5768038f
Osaro Iyoha <saro.iyoha@gmail.com>
2024-02-22 12:14:16 +0100

    Add Second screen stack

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -10,7 +11,8 @@ export default function App() {
   <StatusBar  style='dark'/>
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name='Categories' component={CategoriesScreen}/>
+      <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+      <Stack.Screen name='MealsOverview' component={MealsOverviewScreen}/>
     </Stack.Navigator>
   </NavigationContainer>
   
